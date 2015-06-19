9f437047c6adf5bedec8c5d657b5876eb3aae327
B. You <brandonyou44@gmail.com>
2015-06-19 10:07:50 -0400

    Create CrimZonU.php

---
+++ CrimZonU.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+
+<html>
+    
+	<head>
+        
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        
+			<title><?php echo $title; ?></title>
+        
+		<link rel="stylesheet" type="text/css" href="style/Stylesheet.css" />
+    
+	</head>
+    
+
+	<body>
+ 
+       
+	<div id="wrapper">
+  
+          
+	<div id="banner">             
+            </div>
+            
+            
+	<nav id="navigation">
+                
+	<ul id="nav">
+                    
+	<li><a href="home.php">Home</a></li>
+                   
+	<li><a href="CrimZon.php">CrimZon</a></li>
+                    
+	<li><a href="CrimZonU.php">CrimZon U</a></li>
+          
+	<li><a href="CrimZonD.php">CrimZon D</a></li>
+            
+	<li><a href="Fanart.php">Fanart</a></li>
+                
+	</ul>
+            
+	</nav>
+            
+            
+	<div id="content_area">
+           <p>This page is the CrimZon Universe Page</p>     <?php echo $content; ?>
+            </div>
+            
+            
+	<div id="sidebar">
+                
+            </div>
+     
+       
+            
+	<footer>
+       
+         <p>All rights reserved</p>
+            
+	</footer>
+ 
+       
+	</div>
+  
+  
+	</body>
+
+</html>
